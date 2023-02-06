c52e3d208374217a860e5873bf2c09fb376f9b78
Pierre Houdayer <pierre.houdayer@live.fr>
2023-02-06 18:30:00 +0100

    Erased old file reading method

--- model_deform.py
+++ model_deform.py
@@ -205,12 +205,6 @@ def init_1D() :
         r1D, rho1D, *other_var = np.genfromtxt(
             './Models/'+MOD_1D, skip_header=2, unpack=True
         )
-        # entry = np.genfromtxt(
-        #     './Models/'+MOD_1D, skip_header=2, unpack=True
-        #     )
-        # r1D, rho1D, other_var = [
-        #     np.squeeze(x) for x in np.vsplit(entry, (1, 2))
-        #     ]
         _, idx = np.unique(r1D, return_index=True) 
         N = len(idx)
         
